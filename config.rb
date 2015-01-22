809ed3dd6276631c572ebd1d214fe805a7e5602d
Yan Li <mangakjd@gmail.com>
2015-01-22 11:45:37 -0500

    fixes config

--- config.rb
+++ config.rb
@@ -13,7 +13,5 @@ configure :build do
   activate :minify_javascript
   activate :asset_hash
   activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  set :relative_links, true
 end
